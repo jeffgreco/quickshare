e89107901af4d6b12026238eabe84944ff50765f
Jeff Greco <jeffgreco@gmail.com>
2016-09-09 21:05:38 -0500

    adding hashtags to tweet

--- src/build/services/twitter.js
+++ src/build/services/twitter.js
@@ -2,6 +2,7 @@ services_lib['twitter'] = {
     extractParams: function($share_link) {
         var params = _defaultParams($share_link),
         	tweet_body = _getData($share_link, 'tweet-body'),
+        	tweet_hashtags = _getData($share_link, 'tweet-hashtags'),
         	via_username = _getData($share_link, 'via-username');
 
         if(tweet_body) {
@@ -15,7 +16,7 @@ services_lib['twitter'] = {
         return params;
     },
     makeUrl: function(params) {
-        var href_url = 'https://twitter.com/intent/tweet?url=' + params.src_url + _mightInclude('&text=', params.tweet_body) + _mightInclude('&via=', params.via_username);
+        var href_url = 'https://twitter.com/intent/tweet?url=' + params.src_url + _mightInclude('&text=', params.tweet_body) + _mightInclude('&via=', params.via_username)  + _mightInclude('&hashtags=', params.tweet_hashtags);
         href_url = "javascript:window.open('" + encodeURIComponent(href_url) + "','myTwitterWin','width=620,height=350'); void(0)";
         return href_url;
     },
